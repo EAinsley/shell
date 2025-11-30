c8649036103a374d5658e5a63f88eec80b9923ee
Ainsley Su <vhtmscyo@gmail.com>
2025-11-30 16:40:39 +0100

    [Stage 5] Add echo and eval.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+
+bool echo(std::stringstream &commands) {
+  std::cout << commands.str();
+  return true;
+}
+
+bool notfound(const std::string command) {
+  std::cout << command << ": command not found";
+  return true;
+}
+
+bool eval(const std::string &commands) {
+  std::stringstream command_ss(commands);
+  std::string command;
+  command_ss >> command;
+  if (command == "exit")
+    return false;
+  if (command == "echo")
+    return echo(command_ss);
+  return notfound(command);
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  std::string command;
+  std::string command_line;
   while (true) {
     std::cout << "$ ";
-    std::getline(std::cin, command);
-    if (command == "exit") {
+    std::getline(std::cin, command_line);
+    if (!eval(command_line))
       break;
-    }
-    std::cout << command << ": command not found" << std::endl;
+    std::cout<<std::endl;
   }
   return 0;
 }
