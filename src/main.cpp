83036115d700c0098faa8c8aaea1248902e7513d
Ainsley Su <vhtmscyo@gmail.com>
2025-11-30 17:14:53 +0100

    [Stage 5] Fix echo output echo. Use `string_view` instead.
    I'm doing it wrong because I'm not familiar with `string_view` and I
    wanted to use the old way that is using `stringstream` to do the
    tokenization. It's convenient but will not avoid copy-paste.
    `string_view` is a reference to the old string with position recorded,
    so that it avoids copy and is more memory efficient. When using
    `string_view`, be sure to check the lifetime of the original string.

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,28 @@
+#include <cstddef>
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <string_view>
 
-bool echo(std::stringstream &commands) {
-  std::cout << commands.str();
+bool echo(const std::string_view &args) {
+  std::cout << args;
   return true;
 }
 
-bool notfound(const std::string command) {
+bool notfound(const std::string_view command) {
   std::cout << command << ": command not found";
   return true;
 }
 
-bool eval(const std::string &commands) {
-  std::stringstream command_ss(commands);
-  std::string command;
-  command_ss >> command;
+bool eval(const std::string_view &commands) {
+  size_t delimiter = commands.find(' ');
+  std::string_view command = commands.substr(0, delimiter);
+  std::string_view args = (delimiter != std::string_view::npos)
+                              ? commands.substr(delimiter + 1)
+                              : std::string_view{};
   if (command == "exit")
     return false;
   if (command == "echo")
-    return echo(command_ss);
+    return echo(args);
   return notfound(command);
 }
 
@@ -33,7 +36,7 @@ int main() {
     std::getline(std::cin, command_line);
     if (!eval(command_line))
       break;
-    std::cout<<std::endl;
+    std::cout << std::endl;
   }
   return 0;
 }
