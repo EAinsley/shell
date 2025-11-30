6cf28c7f77a732a8f5a9a3cee0c7ac1c0bf8fbfb
Ainsley Su <vhtmscyo@gmail.com>
2025-11-30 16:08:56 +0100

    [stage 2] Handle invalid command.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  std::string command;
   std::cout << "$ ";
+  std::getline(std::cin, command);
+  std::cout << command << ": command not found" << std::endl;
 }
