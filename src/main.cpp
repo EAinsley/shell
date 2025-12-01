f733061161b03822a170cbdf4792998a1984d52b
Ainsley Su <vhtmscyo@gmail.com>
2025-12-01 12:52:45 +0100

    [Stage 5] implement type.

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,68 @@
+#include <array>
 #include <cstddef>
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <utility>
 
-bool echo(const std::string_view &args) {
-  std::cout << args;
-  return true;
+using function_handle_t = int (*)(std::string_view args);
+// eval and build
+
+int echo_handler(std::string_view args);
+int type_handler(std::string_view args);
+int exit_handler(std::string_view _);
+
+constexpr auto handler_map =
+    std::array<std::pair<std::string_view, function_handle_t>, 3>{{
+        {"echo", echo_handler},
+        {"type", type_handler},
+        {"exit", exit_handler},
+    }};
+
+function_handle_t get_builtin(std::string_view name) {
+  for (auto [key, handler] : handler_map) {
+    if (key == name)
+      return handler;
+  }
+  return NULL;
 }
 
-bool notfound(const std::string_view command) {
+bool notfound(std::string_view command) {
   std::cout << command << ": command not found";
   return true;
 }
 
-bool eval(const std::string_view &commands) {
+int eval(std::string_view commands) {
   size_t delimiter = commands.find(' ');
   std::string_view command = commands.substr(0, delimiter);
   std::string_view args = (delimiter != std::string_view::npos)
                               ? commands.substr(delimiter + 1)
                               : std::string_view{};
-  if (command == "exit")
-    return false;
-  if (command == "echo")
-    return echo(args);
+  function_handle_t func_handler = get_builtin(command);
+  if (func_handler != NULL) {
+    return func_handler(args);
+  }
   return notfound(command);
 }
+
+// Built-in Functions
+int echo_handler(std::string_view args) {
+  std::cout << args;
+  return 0;
+}
+
+int type_handler(std::string_view args) {
+  size_t first_token_pos = args.find(" ");
+  std::string_view first_arg = args.substr(0, first_token_pos);
+  if (get_builtin(first_arg)) {
+    std::cout << first_arg << " is a shell builtin";
+  } else {
+    std::cout << first_arg << ": not found";
+  }
+  return 0;
+}
+
+int exit_handler(std::string_view _) { return 9; }
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -34,7 +72,8 @@ int main() {
   while (true) {
     std::cout << "$ ";
     std::getline(std::cin, command_line);
-    if (!eval(command_line))
+    int flag = eval(command_line);
+    if (flag == 9)
       break;
     std::cout << std::endl;
   }
